43fa16ab727dbb253c460bbb21ad81bbbadd93a7
Scar-Galilea <floresmariana475@gmail.com>
2024-10-14 17:24:00 -0600

    Actualizo datos Y se mas agrego código además que se corrigieron errores ortograficos

--- Fundamento-Python/Tipos-de-datos.py
+++ Fundamento-Python/Tipos-de-datos.py
@@ -3,7 +3,6 @@
 #Descripción:
 #Usos de los tipos básico de datos en Python.
 
-
 # Notas:
 """
 En Python, no se requiere indicar el tipo de la variable en su declaración.
@@ -14,3 +13,38 @@ Los valores básicos que pueden almacenar las variables son:
 - Boolean. True o False (con inicial mayúscula).
 - None. Tipo especial que representa una ausencia de valor.
 """
+
+# Ejemplos de tipos de datos.
+# Número entero:
+mi_variable_entera = -777
+print("Tipo de dato entero:",mi_variable_entera)
+
+# Número decimal:
+mi_variable_decimal = 43
+print("Tipo de dato decimal:", mi_variable_decimal)
+
+# Cadena de texto:
+mi_variable_texto_nombre = "Galilea"
+mi_variable_texto_apellido = 'Peralta'
+print("Cadena de texto:", mi_variable_texto_nombre, mi_variable_texto_apellido)
+
+# Booleno:
+mi_variable_booleana = True
+print('Tipo booleano:', mi_variable_booleana)
+
+# None:
+mi_variable_none = None
+print("Tipo none:",mi_variable_none)
+
+# Uso de constantes.
+'''
+En Python, a diferencia de otros lenguajes de programación, no existe un tipo específico para definir constantes.
+Se utiliza una convención de colocar las variables en mayúsculas y no modificarlas.
+'''
+VARIABLE_CONSTANTE = 3.1416
+print("Ejemplo de convención de una constante:", VARIABLE_CONSTANTE)
+
+#Notas:
+#Para poder identificar el tipo de mis variables entendí que el nombre de la variable debe de ser mas concreto.
+#En Python en mi opinión, se facilita el uso de variables.
+#La variable None es refencia a NULL de C++.
