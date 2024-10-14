c10ba3b41e7db1030e7651af3d981730008552df
Scar-Galilea <floresmariana475@gmail.com>
2024-10-14 14:49:24 -0600

    Se agregó un nuevo archivo llamado Hola_mundo

--- Fundamento-Python/Tipos-de-datos.py
+++ Fundamento-Python/Tipos-de-datos.py
@@ -1,1 +1,9 @@
-c
+#Galilea Peralta Contreras.
+#8 de octubre de 2024.
+#En este archivo solamente imprimirá el mensaje de "hola mundo" el cual se mostrará en pantalla.
+
+print("Hola Mundo")
+#Notas:
+# #La función print sirve para imprimir.
+#En Python ya no lleva punto y coma en los códigos.
+#Además en Python solo se necesita  la función print y ya no su biblioteca stdio.h.
