16fef2139c760289ef09fa5efc211fb6753c4446
Scar-Galilea <floresmariana475@gmail.com>
2024-10-14 14:51:35 -0600

    Actualizo datos Y se agrego código

---
+++ Fundamento-Python/Hola_mundo.py
@@ -0,0 +1,9 @@
+#Galilea Peralta Contreras.
+#8 de octubre de 2024.
+#En este archivo solamente imprimirá el mensaje de "hola mundo" el cual se mostrará en pantalla.
+
+print("Hola Mundo")
+#NotEs:
+# #La función print sirve para imprimir.
+#En Python ya no lleva punto y coma en los códigos.
+#Además en Python solo se necesita  la función print y ya no su biblioteca stdio.h.
