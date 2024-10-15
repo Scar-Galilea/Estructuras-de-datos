d5830b86775335936b5fdcf717149a63627279ac
Scar-Galilea <floresmariana475@gmail.com>
2024-10-14 20:51:47 -0600

    Se agrego codigo

--- Fundamento-Python/Casting.py
+++ Fundamento-Python/Casting.py
@@ -3,7 +3,7 @@
 #Descripción:
 #Conversión de tipos de datos (casting) en Python.
 
-# Notas:
+# Notas
 '''
 La conversión de tipos de datos implica manipular datos que no están en el tipo de dato requerido. Ejemplos:
 de cadena a entero, de cadena a número flotante, y viceversa.
@@ -12,16 +12,15 @@ de cadena a entero, de cadena a número flotante, y viceversa.
 # *****   Conversión de cadena a entero     *****
 var_cadena = "951"
 var_int = int(var_cadena)
+# La función int() convierte una cadena de texto que contiene un número entero en un valor de tipo entero (int).
+# Si la cadena no representa un número entero válido, Python lanzará un error.
 
-#Notas
 # Utiliza la letra "f" antes de las comillas para indicar que la cadena será formateada.
 # Esto significa que puedes incluir variables y expresiones dentro de las llaves {}
 # y su valor será insertado en el texto final.
-
 print("Conversión de cadena a entero.")
 print(f"Número como cadena: {var_cadena}.")
 print(f"Número como int más uno: {var_int + 1}.")
-
 
 # *****   Conversión de cadena a flotante     *****
 var_cadena = "8.88"
@@ -30,7 +29,7 @@ print()
 print("Conversión de cadena a flotante.")
 print(f"Número como cadena: {var_cadena}.")
 print(f"Número como float más cero punto uno: {var_float + 0.1}.")
-
+# La función float() convierte una cadena de texto que contiene un número con decimales o formato flotante.
 
 # *****   Conversión de número a cadena     *****
 var_int = 123
@@ -39,16 +38,16 @@ print()
 print("Conversión de número a cadena.")
 print(f"Los números {var_int} y {var_float} se convierten a cadena utilizando str(var_int): {str(var_cadena)}, y "
       f"str(var_float): {str(var_float)}.")
-
+# La función str() convierte un número, ya sea entero o flotante, en su representación como cadena de texto.
 
 # *****   Conversión a booleano     *****
 # Si el valor es 0, cadena vacía o None, la función bool regresa un valor de False. En caso contrario, regresa True.
 print()
 print("Conversión a booleanos.")
-
 var_int = 0
 es_verdadero = bool(var_int)
 print(f"¿El valor de {var_int} es verdadero? {es_verdadero}.")
+
 var_int = -30
 es_verdadero = bool(var_int)
 print(f"¿El valor de {var_int} es verdadero? {es_verdadero}.")
@@ -56,6 +55,7 @@ print(f"¿El valor de {var_int} es verdadero? {es_verdadero}.")
 var_float = 0.0
 es_verdadero = bool(var_float)
 print(f"¿El valor de {var_float} es verdadero? {es_verdadero}.")
+
 var_float = 0.01
 es_verdadero = bool(var_float)
 print(f"¿El valor de {var_float} es verdadero? {es_verdadero}.")
@@ -63,9 +63,22 @@ print(f"¿El valor de {var_float} es verdadero? {es_verdadero}.")
 var_cadena = ""
 es_verdadero = bool(var_cadena)
 print(f"¿El valor de {var_cadena} es verdadero? {es_verdadero}.")
+
 var_cadena = None
 es_verdadero = bool(var_cadena)
 print(f"¿El valor de {var_cadena} es verdadero? {es_verdadero}.")
+
 var_cadena = " "
 es_verdadero = bool(var_cadena)
 print(f"¿El valor de {var_cadena} es verdadero? {es_verdadero}.")
+
+# Los siguientes valores siempre se convierten a False:
+# - 0
+# - 0.0
+# - Una cadena vacía ("")
+# - El valor None
+# Todos los demás valores, como números no nulos o cadenas no vacías, se convierten a True.
+
+Notas:
+# En Python, las funciones int(), float(), str() y bool() se utilizan comúnmente para realizar estas conversiones.
+
