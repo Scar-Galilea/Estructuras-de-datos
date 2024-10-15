33a002bd5223740312968bcc9966318cb55cb6ff
Scar-Galilea <floresmariana475@gmail.com>
2024-10-14 20:21:31 -0600

    Corrección del código

---
+++ Fundamento-Python/Tipos_de_datos_ejercicio.py
@@ -0,0 +1,36 @@
+#Galilea Peralta Contreras.
+#11 de octubre de 2024.
+#Descripción:
+#Ejercicio
+
+#Rellenar datos.
+Día = "lunes"
+Pasaje = 20
+Comida = 135.5
+Otros_gastos = 34.5
+Pregunta_del_presupuesto = True
+
+#Mostrar datos
+print("*** Gastos diarios ***")
+print("Día:",Día)
+print("Pasaje:",Pasaje)
+print("Comida:",Comida)
+print("Otros gastos:",Otros_gastos)
+print("¿Fue mayor a mi presupuesto?",Pregunta_del_presupuesto)
+
+#Modificar datos:
+print()
+Día = "martes"
+Pasaje = 12
+Comida = 45.5
+Otros_gastos = 4.5
+Pregunta_del_presupuesto = False
+
+#Mostrar datos modificados:
+print()#Espacio.
+print("*** Gastos diarios ***")
+print("Día:",Día)
+print("Pasaje:",Pasaje)
+print("Comida:",Comida)
+print("Otros gastos:",Otros_gastos)
+print("¿Fue mayor a mi presupuesto?",Pregunta_del_presupuesto)
