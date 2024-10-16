b7b1297848151947fffb043724108b130401fda3
Scar-Galilea <floresmariana475@gmail.com>
2024-10-16 15:29:17 -0600

    Se agrego nuevo archivo

---
+++ Operadores_en_Python/Operadores_asignación_compuesta.py
@@ -0,0 +1,46 @@
+#Galilea Peralta Contreras.
+#16 de octubre de 2024.
+#Descripción:
+#Usos de los tipos básico de datos en Python.
+
+Variable_1= input("Ingrese un numero: ")
+Variable_1=int(Variable_1)
+
+Variable_2= input("Ingrese un numero: ")
+Variable_2=int(Variable_2)
+
+Variable_1 += 3
+Variable_2 -= 5
+Variable_1 *= 2
+Variable_2 /= 4
+print(f"El resultado del numero 1 es: {Variable_1} y del numero 2 es {Variable_2}.")
+
+print()
+Variable_1= input("Ingrese un numero: ")
+Variable_1=int(Variable_1)
+
+Variable_2= input("Ingrese un numero: ")
+Variable_2=int(Variable_2)
+
+Variable_1 += Variable_2
+Variable_1 *= Variable_1
+Variable_1 -= Variable_2
+Variable_1 += 3
+Variable_1 /= 2
+
+print(f"El resultado del numero 1 es: {Variable_1} y del numero 2 es {Variable_2}.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
