68e01493651d81ef04a02c4613c105c7971ca26c
Scar-Galilea <floresmariana475@gmail.com>
2024-10-16 15:56:25 -0600

    Se agrego codigo

---
+++ Operadores_en_Python/Operadores_relacionales.py
@@ -0,0 +1,15 @@
+#Galilea Peralta Contreras.
+#16 de octubre de 2024.
+#Descripción:
+#Usos de los tipos básico de datos en Python.
+#Se utilizan para comparar dos valores
+
+Variable_1= 87
+Variable_2= 42.7
+
+print(f"¿El número {Variable_1} es mayor a {Variable_2}? {Variable_1 > Variable_2}")
+print(f"¿El número {Variable_1} es mayor igual a {Variable_2}? {Variable_1 >= Variable_2}")
+print(f"¿El número {Variable_1} es igual a {Variable_2}? {Variable_1 == Variable_2}")
+print(f"¿El número {Variable_1} es menor a {Variable_2}? {Variable_1 < Variable_2}")
+print(f"¿El número {Variable_1} es menor igual a {Variable_2}? {Variable_1 <= Variable_2}")
+print(f"¿El número {Variable_1} es diferente a {Variable_2}? {Variable_1 != Variable_2}")
