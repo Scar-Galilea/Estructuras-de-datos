aeb990a43eaa845d9cb67b6dbeb5c7976c1764de
Scar-Galilea <floresmariana475@gmail.com>
2024-10-16 16:05:14 -0600

    Se agrego codigo

--- Operadores_en_Python/Operadores_logicos.py
+++ Operadores_en_Python/Operadores_logicos.py
@@ -5,10 +5,7 @@
 #Se utilizan para comparar dos valores
 
 Espresión_1= input("Ingrese un Si o No: ")
-Variable_1=bool(Espresión_1)
-
 Espresión_2= input("Ingrese un Si o No: ")
-Espresión_2=bool(Espresión_2)
 
 Espresión_1 = Espresión_1.lower()=="No"
 Espresión_2 = Espresión_2.lower()=="No"
