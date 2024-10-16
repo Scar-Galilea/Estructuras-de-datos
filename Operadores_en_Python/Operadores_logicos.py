0f8d022893bc23699d667ddb3c1434db49e03b4f
Scar-Galilea <floresmariana475@gmail.com>
2024-10-16 15:56:18 -0600

    Se agrego codigo

---
+++ Operadores_en_Python/Operadores_logicos.py
@@ -0,0 +1,15 @@
+#Galilea Peralta Contreras.
+#16 de octubre de 2024.
+#Descripción:
+#Usos de los tipos básico de datos en Python.
+#Se utilizan para comparar dos valores
+
+Espresión_1= input("Ingrese un Si o No: ")
+Variable_1=bool(Espresión_1)
+
+Espresión_2= input("Ingrese un Si o No: ")
+Espresión_2=bool(Espresión_2)
+
+print(Espresión_1)
+print(Espresión_2)
+
