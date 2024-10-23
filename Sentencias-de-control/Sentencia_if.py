eed315a3655e156e45302a4f27b048b24990e9e8
Scar-Galilea <floresmariana475@gmail.com>
2024-10-23 16:04:06 -0600

    Se agrego codigo y ejercicios

---
+++ Sentencias-de-control/Sentencia_if.py
@@ -0,0 +1,13 @@
+#Galilea Peralta Contreras.
+#23 de octubre de 2024.
+#Descripción:
+#Usos de los tipos básico de datos en Python.
+#Se utilizan para comparar dos valores
+
+print("*** Programa que determina si es mayor de edad +++")
+
+Variable_1 = input("Ingresa tu edad: ")
+Variable_1 = int(Variable_1)
+if Variable_1 >= 18 :
+# Código a ejecutar si la condición es verdadera
+    print("Eres mayor de edad")
