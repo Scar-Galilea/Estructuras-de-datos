3a6e97a77aebe4f0d7e510033d4f45f96f5af669
Scar-Galilea <floresmariana475@gmail.com>
2024-10-29 16:03:26 -0600

    Se agrego codigo y ejercicios

---
+++ Ciclos/Ciclos_ejercicio_3.py
@@ -0,0 +1,70 @@
+#Galilea Peralta Contreras.
+#29 de octubre de 2024.
+#Descripción:
+#Usos de los tipos básico de datos en Python.
+
+Contador_1 = 1
+while Contador_1 != 0:
+    print("*** Calculadora básica **+")
+    print("0) Salir")
+    print("1) Suma")
+    print("2) Resta")
+    print("3) Multipicación")
+    print("4) Division")
+    print("5) Division entera")
+    print("6) Exponenciación")
+    print()
+    Variable_1 = int(input("Ingresa una opción: "))
+    Contador_1 = Variable_1
+
+    if Variable_1 == 1:
+        print()
+        print("Suma:")
+        Variable_2 = float(input("Ingrese un número: "))
+        Variable_3 = float(input("Ingrese otro número: "))
+        print()
+        print(f"La suma entre el {Variable_2} y el {Variable_3} es {Variable_2 + Variable_3}")
+    elif Variable_1 == 2:
+        print()
+        print("Resta:")
+        Variable_2 = float(input("Ingrese un número: "))
+        Variable_3 = float(input("Ingrese otro número: "))
+        print()
+        print(f"La resta entre el {Variable_2} y el {Variable_3} es: {Variable_2 - Variable_3}")
+    elif Variable_1 == 3:
+        print()
+        print("Multiplicación:")
+        Variable_2 = float(input("Ingrese un número: "))
+        Variable_3 = float(input("Ingrese otro número: "))
+        print()
+        print(f"La multiplicación entre el {Variable_2} y el {Variable_3} es: {Variable_2 * Variable_3}")
+    elif Variable_1 == 4:
+        print()
+        print("División:")
+        Variable_2 = float(input("Ingrese un número: "))
+        Variable_3 = float(input("Ingrese otro número: "))
+        print()
+        print(f"La división entre el {Variable_2} y el {Variable_3} es: {Variable_2 / Variable_3}")
+    elif Variable_1 == 5:
+        print()
+        print("División entera:")
+        Variable_2 = float(input("Ingrese un número: "))
+        Variable_3 = float(input("Ingrese otro número: "))
+        print()
+        print(f"La división entre el {Variable_2} y el {Variable_3} es: {Variable_2 // Variable_3}")
+    elif Variable_1 == 6:
+        print()
+        print("Exponenciacion:")
+        Variable_2 = float(input("Ingrese un número: "))
+        Variable_3 = float(input("Ingrese otro número: "))
+        print()
+        print(f"La exponenciacion entre el {Variable_2} y el {Variable_3} es: {Variable_2 ** Variable_3}")
+    else:
+        if Variable_1 != 0:
+            print()
+            print("Opción incorrecta")
+            print()
+print()
+print("Salio del programa")
+
+
---
+++ Ciclos/Ciclos_ejercicio_4.py
@@ -0,0 +1,42 @@
+#Galilea Peralta Contreras.
+#29 de octubre de 2024.
+#Descripción:
+#Usos de los tipos básico de datos en Python.
+
+Saldo = 0
+Contador_1 = 1
+while Contador_1 != 0:
+    print("*** Bienvenido al Banco Azteca **+")
+    print("0) Salir")
+    print("1) Consulte saldo")
+    print("2) Ingresar dinero")
+    print("3) Retirar dinero")
+    print()
+    Variable_1 = int(input("Ingresa una opción: "))
+    Contador_1 = Variable_1
+
+    if Variable_1 == 1:
+        print()
+        print(f"Su saldo es de: ${Saldo}")
+        print()
+    elif Variable_1 == 2:
+        Variable_2 = float(input("Ingrese la cantidad: "))
+        Saldo =  Saldo + Variable_2
+        print("Se ingreso su saldo exitosamente")
+        print()
+    elif Variable_1 == 3:
+        Variable_2 = float(input("Ingrese la cantidad que se va a retirar: "))
+        if Variable_2 < Saldo:
+            Saldo = Saldo - Variable_2
+            print("Se retiro su saldo exitosamente")
+        else:
+            print("No se pudo completar esta operación")
+        print()
+    else:
+        if Variable_1 != 0:
+            print()
+            print("Opción incorrecta")
+            print()
+print()
+print("Salio del programa")
+
