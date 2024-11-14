d9b18a34ec7a9675f5cf7b0a90c26617e9fc5a26
Scar-Galilea <floresmariana475@gmail.com>
2024-11-14 16:23:02 -0600

    Se agrego nuevos archivos

--- Funciones_y_colecciones/Listas_ejercicio_3_promedios.py
+++ Funciones_y_colecciones/Listas_ejercicio_3_promedios.py
@@ -2,7 +2,15 @@
 #13 de noviembre del 2024.
 #Descripción:
 #Usos de los tipos básico de datos en Python
-Calificaciones =
+Estructura_de_datos = []
+Derecho = []
+Contabilidad = []
+Algebra = []
+Electronica = []
+Calificaciones = [Estructura_de_datos, Derecho, Contabilidad, Algebra, Electronica]
+Posicion = 0
+Contador_1 = 1
+
 while Contador_1 != 0:
     print("*** Promedios del Parcial 1**+")
     print()
@@ -16,22 +24,31 @@ while Contador_1 != 0:
     Contador_1 = int(input("Ingresa una opción: "))
     if Contador_1  != 0:
         if Contador_1  == 1:
-            Numero = 0
-            for Producto in Productos:
-                print(f"{Cantidades[Numero]}- {Producto}")
-                Numero += 1
+            print()
+            for Calificacion in Calificaciones:
+                print(f" {Calificacion}")
+            print()
         elif Contador_1 == 2:
-            Numero = 0
-            for Producto in Productos:
-                print(f"{Cantidades[Numero]}- {Producto}")
-                Numero += 1
+            print()
+            for Calificacion in Calificaciones:
+                print(f" {Calificacion}")
+            print()
         elif Contador_1  == 3:
             print()
-            Producto_añadido = input("Ingrese el producto: ")
-            Cantidad_añadida = input("Ingrese la cantidad del producto: ")
-            Productos.append(Producto_añadido)
-            Cantidades.append(Cantidad_añadida)
-            Contador_2 += 1
+            print("Ingrese las calificaciones del alumno")
+            Estructura_añadido = float(input("Estructuras de datos: "))
+            Derecho_añadido = float(input("Derecho: "))
+            Contabilidad_añadido = float(input("Contabilidad: "))
+            Algebra_añadido = float(input("Algebra: "))
+            Electronica_añadido = float(input("Electronica: "))
+
+            Calificaciones[0].append(Estructura_añadido)
+            Calificaciones[1].append(Derecho_añadido)
+            Calificaciones[2].append(Contabilidad_añadido)
+            Calificaciones[3].append(Algebra_añadido)
+            Calificaciones[4].append(Electronica_añadido)
+
+            Posición = +1
             print()
         elif Contador_1  == 4:
             Eliminar_producto = input("Ingrese el producto que desea eliminar: ")
