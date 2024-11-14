b8e0bec0e70a4c052845afc6bc89ad11339a01b4
Scar-Galilea <floresmariana475@gmail.com>
2024-11-14 16:22:56 -0600

    Se agrego nuevos archivos

--- Funciones_y_colecciones/Listas_ejercicio_2_compras.py
+++ Funciones_y_colecciones/Listas_ejercicio_2_compras.py
@@ -2,11 +2,7 @@
 #13 de noviembre del 2024.
 #Descripción:
 #Usos de los tipos básico de datos en Python
-Productos = []
-Cantidades = []
-Contador_1 = 1
-Contador_2 = 0
-while Contador_1 != 0:
+def Menu():
     print("*** Listas de compras**+")
     print()
     print("0) Salir")
@@ -14,22 +10,38 @@ while Contador_1 != 0:
     print("2) Añadir producto a la lista")
     print("3) Eliminar producto de la lista")
     print()
-    Contador_1 = int(input("Ingresa una opción: "))
-    if Contador_1  != 0:
-        if Contador_1  == 1:
+
+    Opcion = int(input("Ingrese la opcion: "))
+    return Opcion
+
+
+Productos = []
+Cantidades = []
+Opcion = None
+
+while Opcion != 0:
+    Opcion = Menu()
+    if Opcion == 0:
+        print("Fin del programa")
+    elif Opcion == 1:
+        print()
+        if len(Productos) == 0:
             Numero = 0
             for Producto in Productos:
                 print(f"{Cantidades[Numero]}- {Producto}")
                 Numero += 1
-        elif Contador_1  == 2:
+        else:
+            print("No hay compras por ver")
             print()
-            Producto_añadido = input("Ingrese el producto: ")
-            Cantidad_añadida = input("Ingrese la cantidad del producto: ")
-            Productos.append(Producto_añadido)
-            Cantidades.append(Cantidad_añadida)
-            Contador_2 += 1
-            print()
-        elif Contador_1  == 3:
+    elif Opcion  == 2:
+        print()
+        Producto_añadido = input("Ingrese el producto: ")
+        Cantidad_añadida = input("Ingrese la cantidad del producto: ")
+        Productos.append(Producto_añadido)
+        Cantidades.append(Cantidad_añadida)
+        print()
+    elif Opcion  == 3:
+        if len(Productos) == 0:
             Eliminar_producto = input("Ingrese el producto que desea eliminar: ")
             Numero_de_producto = 0
 
@@ -38,10 +50,13 @@ while Contador_1 != 0:
 
             Productos.remove(Eliminar_producto)
             del Cantidades[Numero_de_producto]
-
         else:
+            print("No hay compras por eliminar")
             print()
-            print("Opción incorrecta")
-            print()
+
+    else:
+        print()
+        print("Opción incorrecta")
+        print()
 print()
 print("Salio del programa")
