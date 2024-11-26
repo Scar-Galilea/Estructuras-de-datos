60735324659180bdfef3c289225917ace6bb3ce5
Scar-Galilea <floresmariana475@gmail.com>
2024-11-26 00:18:58 -0600

    Comentarios

--- Funciones_y_colecciones/Listas_ej1_playlist.py
+++ Funciones_y_colecciones/Listas_ej1_playlist.py
@@ -1,7 +1,7 @@
 #Galilea Peralta Contreras.
 #12 de noviembre del 2024.
 #Descripción:
-#Usos de los tipos básico de datos en Python
+#Este programa es una playlist de videos de YouTube, en la que se pueden realizar diversas operaciones.
 """
 Escribe un programa de nombre Listas_ej1_playlist.py que realice lo siguiente:
 Este programa es una playlist de videos de YouTube.
@@ -21,6 +21,7 @@ a) Utilice funciones para modular el código.
 b) Utilice una lista para la playlist.
 c) Utilice índices para mostrar los videos.
 """
+# Función para mostrar el menú y obtener la opción seleccionada por el usuario.
 def Menu():
     print("*** Playlist de videos de YouTube **+")
     print()
@@ -35,49 +36,50 @@ def Menu():
     Opcion = int(input("Ingresa una opción: "))
     return Opcion
 
-Videos_de_YT = []
-Opcion = None
+#Inicialización de la lista de videos y la variable para controlar el menú.
+Videos_de_YT = []   #Lista que almacena los videos.
+Opcion = None       #Variable para la opción seleccionada.
 
 while Opcion != 0:
-    Opcion = Menu()
+    Opcion = Menu()  #Llamada de la función del menú y almacena la opción seleccionada.
     if Opcion  == 0:
         print()
         print("Fin del programa.")
-    elif Opcion  == 1:
-        Numero = 1
+    elif Opcion  == 1: #Ver la playlist por orden de añadido.
+        Numero = 1 #Contador para enumerar los videos.
         for Video_de_YT in Videos_de_YT:
             print(f"{Numero}._ {Video_de_YT}")
             Numero += 1
-    elif Opcion  == 2:
+    elif Opcion  == 2:  #Ver la playlist en orden ascendente (A-Z).
         print()
-        Videos_de_YT.sort()
+        Videos_de_YT.sort() #Ordena la lista alfabéticamente.
         for Video_de_YT in Videos_de_YT:
             print(Video_de_YT, end=",")
             print()
-    elif Opcion  == 3:
+    elif Opcion  == 3:   #Ver la playlist en orden descendente (Z-A).
         print()
-        Videos_de_YT.sort(reverse = True)
+        Videos_de_YT.sort(reverse = True) #Ordena la lista en orden inverso.
         for Video_de_YT in Videos_de_YT:
             print(Video_de_YT, end=",")
             print()
-    elif Opcion  == 4:
+    elif Opcion  == 4: #Añadir un video a la playlist.
         print()
         Videos = input("Ingrese el video: ")
         Videos_de_YT.append(Videos)
         print()
-    elif Opcion  == 5:
+    elif Opcion  == 5: #Añadir varios videos a la playlist.
         print()
-        Añadidos = int(input("¿Cuántos videos desea añadir? "))
+        Añadidos = int(input("¿Cuántos videos desea añadir? "))#Solicita la cantidad de videos.
         Contador_2 = 0
-        while Contador_2 <= Añadidos:
+        while Contador_2 <= Añadidos:  #Añade los videos uno por uno.
             Videos = input("Ingrese el video: ")
             Videos_de_YT.append(Videos)
             Contador_2 += 1
         print()
-    elif Opcion  == 6:
+    elif Opcion  == 6:  #Eliminar un video de la playlist.
         Eliminar_video = input("Ingrese el video que desea eliminar: ")
         Videos_de_YT.remove(Eliminar_video)
-    else:
+    else:  #Manejo de opciones no válidas.
         print()
         print("Opción incorrecta.")
         print()
